a59a8c6712bc1a6c34edd37f6d7e29488181fffb
wickedev <orange881217@gmail.com>
2022-01-11 20:49:59 +0900

    VARCHAR to TEXT (no performance difference VARCHAR and TEXT on postgresql)

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,8 +1,8 @@
 -- users
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
-    email VARCHAR(191) UNIQUE NOT NULL,
-    name VARCHAR(191),
+    email TEXT UNIQUE NOT NULL,
+    name TEXT,
     hash_salt VARCHAR(500) NOT NULL,
-    roles VARCHAR(16)[]
+    roles TEXT[]
 );
