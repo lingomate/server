29c8d92cdd585f1722551bc11f4b4bb2f1ae539f
wickedev <orange881217@gmail.com>
2022-01-11 20:59:51 +0900

    hash_salt VARCHAR to TEXT

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     name TEXT,
-    hash_salt VARCHAR(500) NOT NULL,
+    hash_salt TEXT NOT NULL,
     roles TEXT[]
 );
